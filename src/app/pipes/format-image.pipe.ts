89431aad42b2ed8f19ee185aa35452e83b5bfd26
HuynhLoi <caoboiloi@users.noreply.github.com>
2020-10-31 09:55:37 +0700

    update history json

--- src/app/pipes/format-image.pipe.ts
+++ src/app/pipes/format-image.pipe.ts
@@ -2,16 +2,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'formatImage'
+	name: 'formatImage'
 })
 export class FormatImagePipe implements PipeTransform {
 
-	constructor(private _sanitizer: DomSanitizer){}
+	constructor(private _sanitizer: DomSanitizer) { }
 
-  transform(value: any): any {
-  	let imagePath:any = null;
-  	imagePath = this._sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + value);
-    return imagePath;
-  }
+	transform(value: any): any {
+		let imagePath: any = null;
+		imagePath = this._sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + value);
+		return imagePath;
+	}
 
 }
